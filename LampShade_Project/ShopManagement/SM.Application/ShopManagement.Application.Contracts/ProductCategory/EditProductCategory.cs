3ac325987395aeeb0e9d3f8c79bcc4ad5b5d3a9b
Kh.Moh3en <Mohsen.khorrami1990@gmail.com>
2024-05-10 10:10:26 +0200

    Created ArticleCategory 1

---
+++ LampShade_Project/ShopManagement/SM.Application/ShopManagement.Application.Contracts/ProductCategory/EditProductCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public class EditProductCategory : CreateProductCategory
+    {
+        public long Id { get; set; }
+
+    }
+
+}
+
---
+++ LampShade_Project/ShopManagement/SM.Domain/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+	public interface IProductCategoryRepository
+	{
+		void Create(ProductCategory entity);
+		ProductCategory GetBy(long Id);
+		List<ProductCategory> GetAll();
+
+	}
+}
+
