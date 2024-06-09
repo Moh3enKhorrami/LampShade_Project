16b37a26885d63ecc3698a1191d9276602afa054
Kh.Moh3en <Mohsen.khorrami1990@gmail.com>
2024-06-09 18:27:44 +0200

    update

--- LampShade/ServiceHost/obj/Debug/net7.0/ServiceHost.AssemblyInfo.cs
+++ LampShade/ServiceHost/obj/Debug/net7.0/ServiceHost.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31ae6acb46c49fca3c21eda3436518bdc2261aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LampShade/ShopManagement.Configuration/obj/Debug/net7.0/ShopManagement.Configuration.AssemblyInfo.cs
+++ LampShade/ShopManagement.Configuration/obj/Debug/net7.0/ShopManagement.Configuration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopManagement.Configuration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31ae6acb46c49fca3c21eda3436518bdc2261aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopManagement.Configuration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopManagement.Configuration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
